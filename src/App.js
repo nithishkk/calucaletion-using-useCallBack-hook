9e88181e3a4819024281a18f70a6d8f84f735fc1
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-04-10 19:08:57 +0530

    added useEffect calucation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import "./style.css";
 // let total = `Total: ${(price * (1 + VAT)).toFixed(5)}`;
 
 export default function App() {
-const Add=useCallback((price,val)=>{
-  const total=`Total: ${(price*(1+val).toFixed(2))}`
-  return total
-},[])
+  const[value,setValue]=useState('')
+useEffect(()=>{
+  function Add(price,val){
+    const total=`Total: ${(price*(1+val).toFixed(2))}`
+    setValue(total)
+  }
+  Add(10,0.25)
+
+  },[])
+// const Add=useCallback((price,val)=>{
+//   const total=`Total: ${(price*(1+val).toFixed(2))}`
+//    //setValue(total)
+//    return total
+//   // setValue(total)
+//   // return total
+// },[value])
   return (
     <div>
-      <h1>{Add(10,0.26)}</h1>
+      {/* <h1>{Add(10,0.26)}</h1> */}
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>{value}</h1>
+      <p>Start editing to see some magic happen {value} :)</p>
     </div>
   );
 }
